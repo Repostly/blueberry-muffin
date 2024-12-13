4509f74f8b6a051e4afafe56e0fbff83b184c4ed
ouckah <aidan.ouckama@gmail.com>
2024-12-13 01:32:17 -0500

    toaster to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import { NextAuthProvider } from './providers'
 import "./globals.css";
 import { Header } from "./components/header";
+import { Toaster } from "@/components/ui/toaster"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
          <NextAuthProvider>
           <Header />
           {children}
+          <Toaster />
          </NextAuthProvider>
       </body>
     </html>
